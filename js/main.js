cb8dbc88310c205a7befffd5b02f2f8a5a30bee5
keanna21 <keannathompson@yahoo.com>
2023-03-07 11:07:30 -0600

    Slot Machine

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const slotImages = ['one', 'two', 'three'];
+ 
+let bank = 20;
+
+function getSlotImages() {
+    Math.floor(Math.random(slotImages))
+    return 
+}
+
+let btn = document.querySelector('button')
+let  restartBtn = document.getElementById('restart')
+
+
+Button.addEventListner('click')
+// need random function using math.floor & math.random
+//init function (what starts the game)
+//reset function 
+// conditional into a game play function
+//call get random imgs if all 3 images match with money in bank the player win
+// if player reaches 0 in bank then player lose
+// once player wins/lose alrert comes up with player wins or lose
